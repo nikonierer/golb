960b0a8deecf97006a11440cbafa3ce0ff89bd09
Marcel Wieser <github@marcel-wieser.de>
2020-09-20 14:34:37 +0200

    [BUGFIX] Fixes STI configuration for golb models

---
+++ Configuration/Extbase/Persistence/Classes.php
@@ -0,0 +1,49 @@
+<?php
+declare(strict_types = 1);
+
+return [
+    \Greenfieldr\Golb\Domain\Model\Page::class => [
+        'tableName' => 'pages',
+        'properties' => [
+            'authorImage' => [
+                'fieldName' => 'tx_golb_author_image'
+            ],
+            'viewCount' => [
+                'fieldName' => 'tx_golb_view_count'
+            ],
+            'relatedPosts' => [
+                'fieldName' => 'tx_golb_related'
+            ],
+            'postsSorting' => [
+                'fieldName' => 'tx_golb_sorting'
+            ],
+            'limit' => [
+                'fieldName' => 'tx_golb_limit'
+            ],
+            'offset' => [
+                'fieldName' => 'tx_golb_offset'
+            ],
+            'action' => [
+                'fieldName' => 'tx_golb_action'
+            ],
+            'contentElements' => [
+                'fieldName' => 'tx_golb_content_elements'
+            ],
+            'subpages' => [
+                'fieldName' => 'tx_golb_subpages'
+            ],
+            'excludedPages' => [
+                'fieldName' => 'tx_golb_exclude'
+            ],
+            'subCategories' => [
+                'fieldName' => 'tx_golb_sub_categories'
+            ],
+        ],
+    ],
+    \Greenfieldr\Golb\Domain\Model\ContentElement::class => [
+        'tableName' => 'pages',
+    ],
+    \Greenfieldr\Golb\Domain\Model\Category::class => [
+        'tableName' => 'sys_category',
+    ]
+];
