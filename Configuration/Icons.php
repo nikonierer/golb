a92fc869bcbc864217d87fa36c4f144a1f08632f
Marcel Küpper <github@kuepper.net>
2023-05-14 22:01:44 +0200

    [TASK] Moves icon registration into its own file

---
+++ Configuration/Icons.php
@@ -0,0 +1,11 @@
+<?php
+return [
+    'apps-pagetree-post' => [
+        'provider' => \TYPO3\CMS\Core\Imaging\IconProvider\SvgIconProvider::class,
+        'source' => 'EXT:golb/Resources/Public/Icons/apps-pagetree-post.svg'
+    ],
+    'ext-golb-tag' => [
+        'provider' => \TYPO3\CMS\Core\Imaging\IconProvider\SvgIconProvider::class,
+        'source' => 'EXT:golb/Resources/Public/Icons/tx_golb_domain_model_tag.svg'
+    ],
+];
