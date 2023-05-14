4cb931d3ab33b4aa726297754f1dfcb4202d501f
Marcel Küpper <github@kuepper.net>
2023-05-14 21:56:18 +0200

    [TASK] Removes obsolete XCLASS for category model

--- ext_localconf.php
+++ ext_localconf.php
@@ -27,10 +27,6 @@ $boot = function ($packageKey) {
 
     $GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['t3lib/class.t3lib_tcemain.php']['processDatamapClass'][\Greenfieldr\Golb\Hooks\SetBackendLayout::class]
         = \Greenfieldr\Golb\Hooks\SetBackendLayout::class;
-
-    $GLOBALS['TYPO3_CONF_VARS']['SYS']['Objects']['TYPO3\\CMS\\Extbase\\Domain\\Model\\Category'] = [
-        'className' => 'Greenfieldr\\Golb\\Domain\\Model\\Category',
-    ];
 };
 
 $boot('golb');
