290555405ae46d785de1847343f9e52984622213
Marcel Küpper <github@kuepper.net>
2023-05-16 09:36:16 +0200

    [!!!][TASK] Removes SetBackendLayoutHook
    Might get reimplementation as EventListener in the future

--- ext_localconf.php
+++ ext_localconf.php
@@ -24,9 +24,6 @@ $boot = function ($packageKey) {
             \Greenfieldr\Golb\Controller\ViewCountController::class => 'countView'
         ]
     );
-
-    $GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['t3lib/class.t3lib_tcemain.php']['processDatamapClass'][\Greenfieldr\Golb\Hooks\SetBackendLayout::class]
-        = \Greenfieldr\Golb\Hooks\SetBackendLayout::class;
 };
 
 $boot('golb');
